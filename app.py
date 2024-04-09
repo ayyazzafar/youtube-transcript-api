dab5668983b4e76bbbc179a09aad9fa012c7d9a8
ayyazzafar <contact@ayyaztech.com>
2024-04-09 09:55:50 +0500

    Add pytube library and implement get_video_title route

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from pytube import YouTube
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from youtube_transcript_api import YouTubeTranscriptApi
@@ -20,5 +21,30 @@ def get_youtube_transcript():
         return jsonify({"error": str(e)}), 500
 
 
+@app.route('/get_video_title', methods=['GET'])
+def get_video_title_route():
+    video_url = request.args.get('video_url')
+    if not video_url:
+        return jsonify({"error": "No video_url provided"}), 400
+
+    video_title = get_video_title(video_url)
+    if not video_title:
+        return jsonify({"error": "Error retrieving video title"}), 500
+
+    return jsonify({"video_url": video_url, "video_title": video_title})
+
+
+def get_video_title(url):
+    try:
+        video = YouTube(url)
+        return video.title
+    except Exception as e:
+        print(f"Error retrieving video title: {str(e)}")
+        return None
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
+
+
+# terminal command to run this flask app:
